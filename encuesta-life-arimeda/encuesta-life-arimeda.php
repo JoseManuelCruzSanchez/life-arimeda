184ad2685f982cdd1096fcd45942bed93323fa00
JoseManuelCruzSanchez <44446487+JoseManuelCruzSanchez@users.noreply.github.com>
2019-07-25 16:54:21 +0200

    Add files via upload

--- encuesta-life-arimeda/encuesta-life-arimeda.php
+++ encuesta-life-arimeda/encuesta-life-arimeda.php
@@ -22,14 +22,44 @@ add_action('admin_menu', 'crear_menu');
 
 /*******    BTN DESCARGA CSV Y CREACION DEL ARCHIVO CSV ********/
 function funcion_descarga(){
+    $ruta_imagen = '/wp-content/plugins/encuesta-life-arimeda/imagenes/excel.png';
     $ruta_fichero_descarga = "/wp-content/plugins/encuesta-life-arimeda/salida-csv/descarga-csv.xlsx";
-    echo '<a href="' . $ruta_fichero_descarga . '" class="enlace-descarga-csv" download="">Descargar datos encuesta</a>';
+    echo '<div class="wrap">
+            <h1 id="info-usuarios-contestan-encuesta">' . cuantos_usuarios_han_rellenado_la_encuesta() . '</h1>
+            <div class="cont-enlace-descarga-excel">
+                <a href="' . $ruta_fichero_descarga . '" class="enlace-descarga-csv" download="">
+                    <img src="' . $ruta_imagen . '">
+                </a>
+                <p>Descarga Datos Encuesta</p>
+            </div>            
+          </div>';
 
     include($_SERVER['DOCUMENT_ROOT'] . "/wp-content/plugins/encuesta-life-arimeda/includes/funciones_csv.php");
     generar_archivo_csv();
 }
 
-function encolar_los_estilos_del_plugin() {
-    wp_enqueue_style( 'estilos-encuesta', '/wp-content/plugins/encuesta-life-arimeda/css/estilos-encuesta.css' );
+function estilos_frontend() {
+    wp_enqueue_style( 'estilos-encuesta-frontend', '/wp-content/plugins/encuesta-life-arimeda/css/estilos-encuesta.css' );
 }
-add_action( 'wp_enqueue_scripts', 'encolar_los_estilos_del_plugin' );
+add_action( 'wp_enqueue_scripts', 'estilos_front_end' );
+
+function estilos_backend() {
+    wp_enqueue_style( 'estilos-encuesta-backend', '/wp-content/plugins/encuesta-life-arimeda/css/backend-estilos.css' );
+}
+add_action( 'admin_enqueue_scripts', 'estilos_backend' );
+
+function cuantos_usuarios_han_rellenado_la_encuesta(){
+    global $wpdb;
+    $resultado = json_decode(json_encode($wpdb->get_results("select count(*) from encuesta")), true);
+    $resultado = intval($resultado[0]['count(*)']);
+    if ($resultado === 0){
+        return 'Ningún usuario ha contestado a la encuesta todavía';
+    }
+    else if ($resultado === 1){
+        return '1 usuario ha contestado la encuesta';
+    }
+    else if ($resultado > 1){
+        return $resultado . 'usuarios han contestado la encuesta';
+    }
+}
+
